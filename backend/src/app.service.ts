4ac24181220d1e430d9a46b2b3a81239d65f1866
Exanubes <blog@exanubes.com>
2022-03-08 18:19:43 +0100

    Update greeting

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Top O' the morning to ya laddies!";
   }
 }
